2ec06b17416dc80b7cc5d96a003d586743463d5a
IzzySun4es <129731303+IzzySun4es@users.noreply.github.com>
2023-10-31 13:45:15 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+const navbar = document.querySelector(".navbar");
+navbar.querySelector(".toggle").addEventListener("click", () => {
+  navbar.classList.toggle("collapsed");
+});
+window.addEventListener("scroll", e => {
+  let windowY = window.pageYOffset;
+  let navbarHeight = document.querySelector(".navbar").offsetHeight;
+  if (windowY > navbarHeight) navbar.classList.add("sticky");
+  else navbar.classList.remove("sticky");
+});
